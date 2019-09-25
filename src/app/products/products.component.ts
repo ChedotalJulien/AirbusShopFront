768175899e60b348d28e10faeee1abf50f659e84
timothe laude <timothelaude@MacBook-Pro-de-timothe.local>
2019-09-25 16:44:46 +0200

    product by categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,31 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
   response: any;
   products: any;
   tab=[];
-  constructor(private _httpClient: HttpClient) { }
+  constructor(private _httpClient: HttpClient,private route: ActivatedRoute,private router: Router) {
+
+  }
 
   ngOnInit() {
-    this._httpClient.get('http://10.0.4.85:8080/categories').subscribe( (response: any) => {
-      this.response = response._embedded.categories;
-      console.log(this.response)
 
+    this.router.events.subscribe((val) => {
+      if(val instanceof NavigationEnd){
+        let url = val.url
+        console.log(url)
+        let p1 = this.route.snapshot.params.p1
+
+        if(p1==1){
+          this.getProducts('http://10.0.4.85:8080/products/search/selectedProducts')
+        }
+        else if(p1 == 2){
+          let idCat = this.route.snapshot.params.p2
+          this.getProducts('http://10.0.4.85:8080/categories/'+idCat+'/product')
+        }
+        // console.log(this.activatedRoute)
+      }
+      let p1 = this.route.snapshot.params.p1
+
+      if(p1==1){
+        this.getProducts('http://10.0.4.85:8080/products/search/selectedProducts')
+      }
     })
-    this._httpClient.get('http://10.0.4.85:8080/products').subscribe((res: any) => {
-      this.products= res._embedded.products;
-      console.log(this.products)
-    })
+
 
 
 
 
   }
+  private getProducts(url)
+  {
+    this._httpClient.get(url).subscribe((res: any) => {
+      this.products = res._embedded.products;
+      console.log(this.products)
+    })
+
+  }
+
 
 }
